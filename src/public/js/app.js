dfc3ca59032ea8618130c73438f0efbb28d340c7
Yeong-huns <vosxja1@gmail.com>
2024-11-29 21:40:15 +0900

    Server < - > Front WebSocket Chat Test

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,35 @@
-alert("public/js/app.js")
+//alert("public/js/app.js")
+
+//const socket = new WebSocket("http://localhost:3000");
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", ()=>{
+	console.log("웹소켓이 연결되었습니다.")
+});
+
+socket.addEventListener("message", (message)=>{
+	console.log(`받은 메세지: ${message.data}`);
+	const li = document.createElement("li");
+	li.innerText = message.data;
+	messageList.append(li);
+})
+
+socket.addEventListener("close", ()=>{
+	console.log("웹소켓 연결이 해제되었습니다.");
+})
+
+/*setTimeout(() => {
+	socket.send("브라우저 -> 서버로 메세지 전송함");
+}, 5000);*/
+
+function handleSubmit(event){
+	event.preventDefault();
+	const input = messageForm.querySelector("input");
+	socket.send(input.value);
+	input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from "http";
+import WebSocket from 'ws'
 
 /**
  * fileName       : server.js
@@ -23,6 +25,30 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"));
 
 
-const handleListen = () => console.log('3000번 포트에서 실행중');
+const handleListen = () => {
+	console.log('3000번 포트에서 실행중');
+	console.log(__dirname); //C:\WebRTC_practice_project\src
+};
 const port = 3000
-app.listen(port, handleListen);
+//app.listen(port, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// const handleConnection = (socket) => console.log(socket);
+
+const sockets = [];
+
+wss.on("connection", (socket)=> {
+	sockets.push(socket);
+	console.log("브라우저와 연결됨");
+	socket.on("close", () => console.log("브라우저와 연결 해제됨."));
+	socket.on("message", (message) => {
+		console.log(message.toString());
+		//socket.send(message.toString());
+		sockets.forEach(existSocket => existSocket.send(`${message}`));
+	});
+
+});
+
+server.listen(port, handleListen)
